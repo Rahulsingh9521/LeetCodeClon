5284d92815714637ff3b42794dc3525f879dd956
Rahulsingh9521 <rahulsinghtanwar12@gmail.com>
2024-01-15 23:27:57 +0530

    backend init commit

---
+++ application/src/main/java/com/leetcodeclone/application/LccBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leetcodeclone.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LccBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LccBackendApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/com/leetcodeclone/application/LccBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcodeclone.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LccBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
