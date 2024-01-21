13ed00085d5be72f1835d17fab15ebbd30148482
Rahulsingh9521 <rahulsinghtanwar12@gmail.com>
2024-01-21 13:22:23 +0530

    added dipendencies

---
+++ src/pjt/pages/loginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginPage() {
+    return ( 
+        <div>
+            <input type="input" value="Some" />
+            <input type="input" value="Somefr" />
+        </div>
+     );
+}
+
+export default LoginPage;
--- src/pjt/pages/mainPage.jsx
+++ src/pjt/pages/mainPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProblemParent from "./problemParent";
 
 const MainPage = () => {
     return (
         <div>
             it's main page of leetcode
+            <ProblemParent/>
         </div>
     )
 }
